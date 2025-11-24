5746b85ab91218f9788f4443b3bc1ea2f5c11f84
Alfaeran <maaurigar@gmail.com>
2025-11-24 17:57:21 +0700

    prettier code

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,16 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Barang Temu Lost & Found API' });
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
